eac31e2f0692aec22dd939ed3184ae22d570c418
Mikael Mårtensson <123842543+MickeMarten@users.noreply.github.com>
2023-12-04 21:39:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,39 +5,31 @@ const Tjänster = document.querySelector('#ul__Tjänster')
 const Kontakt = document.querySelector('#ul__Kontakt')
 const mainSection = document.querySelector('#main__Section')
 
-  // Method for creating elements with fewer lines
 
- 
-
-
-Referenser.addEventListener('click', ()=>{
-    const sectionUL = document.createElement('ul');
-    sectionUL.setAttribute('id', 'sectionULID' )
-    mainSection.appendChild(sectionUL);
+  class Showtext {
+    constructor() { 
     
+  }
+    createSpan(Företag1, Företag2, Företag3){
+      const referenserLista = [Företag1, Företag2, Företag3];
+      referenserLista.forEach((företag) => {
+      const span = document.createElement('span');
+      span.innerText = företag;
+      mainSection.appendChild(span);
+      })
     
-    for (let index = 0; index < 3; index++) {
-        const referensTag = document.createElement('span');
-        referensTag.innerText='Hallå Haris detta är en loop som körs 3 gånger  för att slippa hämta samma element 3 gånger i koden. =) Klicka igen så försvinner texten.'
-        sectionUL.appendChild(referensTag);
-        }
+   }
+  }
 
-        
-        Referenser.addEventListener('click', () => {
-            const sectionULs = document.querySelector('#sectionULID');
-            sectionULs.remove(); 
-            
-            
-            })
+  const Showtexts = new Showtext;
+  const Showtexts2 = new Showtext;
 
-            
- })
+  Referenser.addEventListener('click', () =>{ 
+    Showtexts.createSpan('Byggmax', 'Schenker', 'K-Rauta')
+  });
 
- 
-
- 
-
-
-
-
+  Tjänster.addEventListener('click', ()=> {
+    Showtexts2.createSpan('Solceller', 'Cevapi', 'Flygspaning')
+  });
+  
 
